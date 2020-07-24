34f7be7a9250efc63c791bbf1d822affda01562c
carlosdaniel-cyber <carlosdanielbp101@gmail.com>
2020-07-24 16:41:37 -0300

    Retornando aos estudos de JavaScript.

---
+++ aula05/script.js
@@ -0,0 +1,15 @@
+// uma única linha
+
+/*
+IDENTIFICADORES
+1. Podem começar com letra, $ ou _
+2. N podem começar com números
+3. É possível usar letras ou números
+4. É possível usar acentos e símbolos
+5. N podem conter espaços
+6. N podem ser palavras reservadas
+*/
+
+var n1 // let n1
+var $n2
+var média
