6467263d849eab368603d2034e3c9f04e2c8c511
carlosdaniel-cyber <carlosdanielbp101@gmail.com>
2020-08-11 12:04:21 -0300

    Aula 13 - estruturas de repetição

---
+++ aula13/environment.js
@@ -0,0 +1,13 @@
+var c = 1
+
+// Estrutura de repetição com teste lógico no início
+while (c <= 10) {
+    console.log(`${c}`)
+    c++
+}
+
+// Estrutura de repetição com teste lógico no final
+do {
+    console.log(`${c}`)
+    c++
+} while (c <= 10)
