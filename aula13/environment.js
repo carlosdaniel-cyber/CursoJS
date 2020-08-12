afd69c078f135474f676346047c67768395b932c
carlosdaniel-cyber <carlosdanielbp101@gmail.com>
2020-08-12 15:37:34 -0300

    Assisti a aula 14

--- aula13/environment.js
+++ aula13/environment.js
@@ -1,4 +1,4 @@
-var c = 1
+/*var c = 1
 
 // Estrutura de repetição com teste lógico no início
 while (c <= 10) {
@@ -10,4 +10,10 @@ while (c <= 10) {
 do {
     console.log(`${c}`)
     c++
-} while (c <= 10)
+} while (c <= 10)*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('fim')
